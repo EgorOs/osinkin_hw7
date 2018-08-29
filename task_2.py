ec7decd429d196e367e599e9f5f7836068f611e5
Egor Osinkin <egoros97@yandex.ru>
2018-08-30 00:25:36 +0300

    attr_name validation added

--- task_2.py
+++ task_2.py
@@ -3,25 +3,23 @@
 
 class EnumMeta(type):
     def __new__(cls, name, bases, dct):
-        _ignore_ = ('__main__', '__module__', '__doc__', '__new__',
-                    '__qualname__', '__init__', '__str__')
+        _valid_ = [n for n in dct.keys() if str(n)[:2] != '__' and str(n)[::-1][:2] != '__']
         _member_to_map_ = {}
         _value2member_map_ = {}
         metacls = cls.__class__
-        for key in dct.keys():
-            if key not in _ignore_:
-                attr_name = str(key)
-                attr_val = dct[key]
-                if attr_val not in _value2member_map_.keys():
-                    # if there is no attribute names assigned to
-                    # this value
-                    _value2member_map_[attr_val] = attr_name
-                    _member_to_map_[attr_name] = attr_val
-                else:
-                    dct[key] = dct[_value2member_map_[attr_val]]
+        
+        for key in _valid_:
+            attr_name = str(key)
+            attr_val = dct[key]
+            if attr_val not in _value2member_map_.keys():
+                # if there is no attribute names assigned to
+                # this value
+                _value2member_map_[attr_val] = attr_name
+                _member_to_map_[attr_name] = attr_val
+            else:
+                dct[key] = dct[_value2member_map_[attr_val]]
 
-        for key in _member_to_map_.keys():
-
+        for key in _valid_:
             dct[key] = metacls.__new__(cls, key, bases,
                                        {'name': attr_name,
                                         'value': attr_val,
@@ -30,7 +28,6 @@ class EnumMeta(type):
                                         '_member_to_map_':_member_to_map_}
                                        )
             # create an instance with valid value
-            # dct[key] = dct[key](_member_to_map_[key], _member_to_map_, _value2member_map_)
             dct[key] = dct[key](_member_to_map_[key])
 
         dct['_value2member_map_'] = _value2member_map_
@@ -54,7 +51,6 @@ class EnumMeta(type):
 
 class Enum(metaclass=EnumMeta):
 
-    # def __new__(cls, val, _member_to_map_=None, _value2member_map_=None):
     def __new__(cls, val):
         if val in cls._value2member_map_.keys():
             key = cls._value2member_map_.get(val)
@@ -72,9 +68,6 @@ class Enum(metaclass=EnumMeta):
     def __str__(self):
             return '<{}.{}: {}>'.format(self.__objclass__, self.name, self.value)
 
-    # def __iter__(self):
-    #     print(self.__dict__)
-    #     yield _member_to_map_.value()
 
 class Direction(Enum):
     north = 0
@@ -83,5 +76,9 @@ class Direction(Enum):
     west = 270
 
 
-for d in Direction:
-    print(d)
+# for d in Direction:
+#     print(d)
+
+# print(id(Direction.north))
+# print(id(Direction['north']))
+# print(id(Direction(0)))
